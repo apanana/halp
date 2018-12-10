3a37bcdef7679e826b012b39fb30c91ab8658701
Alex Pan <apan@flowmill.com>
2018-12-09 18:24:52 -0800

    tests for get_node_set() and node_iterator()

--- tests/test_signaling_hypergraph.py
+++ tests/test_signaling_hypergraph.py
@@ -74,3 +74,46 @@ def test_add_nodes():
     assert node_d in H._node_attributes
     assert H._node_attributes[node_d]['label'] == 'black'
     assert H._node_attributes[node_d]['sink'] is True
+
+def test_get_node_set():
+    node_a = 'A'
+    node_b = 'B'
+    node_c = 'C'
+    attrib_c = {'alt_name': 1337}
+    node_d = 'D'
+    attrib_d = {'label': 'black', 'sink': True}
+    common_attrib = {'common': True, 'source': False}
+
+    node_list = [node_a, (node_b, {'source': False}),
+                 (node_c, attrib_c), (node_d, attrib_d)]
+
+    # Test adding unadded nodes with various attribute settings
+    H = SignalingHypergraph()
+    H.add_nodes(node_list, common_attrib)
+    node_set = H.get_node_set()
+    assert node_set == set(['A', 'B', 'C', 'D'])
+    assert len(node_set) == len(node_list)
+    for node in H.node_iterator():
+        assert node in node_set
+
+def test_node_iterator():
+    node_a = 'A'
+    node_b = 'B'
+    node_c = 'C'
+    attrib_c = {'alt_name': 1337}
+    node_d = 'D'
+    attrib_d = {'label': 'black', 'sink': True}
+    common_attrib = {'common': True, 'source': False}
+
+    node_list = [node_a, (node_b, {'source': False}),
+                 (node_c, attrib_c), (node_d, attrib_d)]
+
+    # Test adding unadded nodes with various attribute settings
+    H = SignalingHypergraph()
+    H.add_nodes(node_list, common_attrib)
+    node_set = H.get_node_set()
+
+    for node in H.node_iterator():
+        assert node in node_set
+
+
